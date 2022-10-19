1a0e3dd4f780a304d4cd5e2c9191eb38de4fe0fc
kanghyew0n <104333249+kanghyew0n@users.noreply.github.com>
2022-10-20 01:06:03 +0900

    feat: cors setting & styled

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Header from "./components/Header";
+import { UploadButton } from "./Style/UploadCss";
 
 function App() {
+  const [text, setText] = useState("");
+  useEffect(() => {
+    let body = {
+      text: "hello",
+    };
+    axios
+      .post("/api/test", body)
+      .then((response) => {
+        console.log("요청성공 : ", response);
+        setText(response.data.text);
+      })
+      .catch((error) => {
+        console.log("요청실패 : ", error);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <div className="App" style={{ textAlign: "center" }}>
+        <h1>멋쟁이 원혜강</h1>
+        <h3>{text}</h3>
+        <UploadButton>button</UploadButton>
+      </div>
+    </>
   );
 }
 
---
+++ client/src/Style/UploadCss.js
@@ -0,0 +1,19 @@
+import styled from "@emotion/styled";
+
+const UploadDiv = styled.div``;
+
+const UploadForm = styled.form``;
+
+const UploadButton = styled.button`
+  padding: 32px;
+  background-color: hotpink;
+  font-size: 24px;
+  border-radius: 4px;
+  color: black;
+  font-weight: bold;
+  &:hover {
+    color: white;
+  }
+`;
+
+export { UploadDiv, UploadForm, UploadButton };
